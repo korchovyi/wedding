14eae6276ffa68354ee4494fc3e58207927acbf4
Andrii Korchovyi <andrii.korchovyi@nobelglobe.com>
2025-08-10 02:30:59 +0300

    init

---
+++ app.js
@@ -0,0 +1,36 @@
+$('.js-start-trigger').on('click', () => {
+  console.log('Start button clicked!');
+
+
+  sealVibrate();
+  setTimeout(() => {
+    sealOpen();
+    setTimeout(() => {
+      $('.js-seal').remove();
+      rollPergament();
+      setTimeout(() => {
+        $('.tassel').addClass('tassel-visible');
+      }, 1000);
+    }, 500);
+  }, 500);
+
+});
+
+const sealVibrate = () => {
+  $('.js-seal').addClass('seal-vibrate seal-shadow').removeClass('seal-hover');
+  setTimeout(() => {
+    $('.js-seal').addClass('seal-shadow-fade');
+  });
+}
+
+const sealOpen = () => {
+  $('.js-seal').removeClass('seal-vibrate').addClass('seal-open');
+}
+
+const rollPergament = () => {
+  $('.js-pergament-top').addClass('pergament-top-opened');
+  $('.js-pergament-bottom').addClass('pergament-bottom-opened');
+  $('.js-pergament-body').addClass('pergament-body-opened');
+}
+
+console.log('App initialized!');
